8fff1610de8aedf3f1fe3c82e295368f5af19216
nerikeshi-k <nerikeshik@gmail.com>
2019-10-16 01:09:43 +0900

    yatta

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
-import * as factory from './snapshot';
-import * as dotenv from 'dotenv';
-import * as server from './server';
+import { createImage } from './painter';
+import { getParams } from './query';
+import express = require('express');
+import fs = require('fs');
+import path = require('path');
 
-dotenv.config();
+const INDEX_HTML_PATH = path.join(__dirname, 'static/html/index.html');
+const PORT = process.env.LGTMI_PORT != null ? parseInt(process.env.LGTMI_PORT, 10) : 3000;
+if (Number.isNaN(PORT)) {
+  process.exit(-1);
+}
 
-const PAGE_LENGTH = Number(process.env.PAGE_LENGTH);
-const PORT = Number(process.env.PORT);
+const indexHTML = fs.readFileSync(INDEX_HTML_PATH).toString();
 
-const main = async () => {
-  await factory.init(PAGE_LENGTH);
-  server.run(PORT);
-};
+const app = express();
 
-main();
+app.get('/', async (req, res) => {
+  const params = getParams(req.query);
+  if (params == null) {
+    res.send(indexHTML);
+    return;
+  }
+  const picture = await createImage(params.url, params.style);
+  if (picture == null) {
+    res.status(400).send('bad request');
+    return;
+  }
+  res.type('png').send(picture);
+});
+
+app.listen(PORT, () => console.log(`🌟 running on port ${PORT}`));
